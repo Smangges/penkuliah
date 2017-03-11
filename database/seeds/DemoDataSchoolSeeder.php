92febd3be128401473c9a2f4605ec0c8f5081527
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2017-03-11 18:52:12 +0700

    Update: more generations 2010-2013

--- database/seeds/DemoDataSchoolSeeder.php
+++ database/seeds/DemoDataSchoolSeeder.php
@@ -21,6 +21,13 @@ class DemoDataSchoolSeeder extends Seeder
             'Biologi'
         ];
 
+        $generations = [
+            2010,
+            2011,
+            2012,
+            2013
+        ];
+
         $subjects = collect([]);
 
         foreach ($subjectNames as $subjectName) {
@@ -31,31 +38,33 @@ class DemoDataSchoolSeeder extends Seeder
 
         $pos = 1;
 
-        factory(School\Student::class, 40)
-            ->create(['generation' => 2010])
-            ->each(function ($student) use ($pos, $subjects) {
-                for ($sem = 1; $sem <= 6; $sem++) {
-                    $rank = factory(School\Rank::class)->make([
-                        'semester' => $sem,
-                        'position' => $pos
-                    ]);
-
-                    $rank->student()->associate($student);
-                    $rank->save();
-
-                    foreach ($subjects as $subject) {
-                        $score = factory(School\Score::class)->make([
-                            'semester' => $sem
+        foreach ($generations as $generation) {
+            factory(School\Student::class, 40)
+                ->create(['generation' => $generation])
+                ->each(function ($student) use ($pos, $subjects) {
+                    for ($sem = 1; $sem <= 6; $sem++) {
+                        $rank = factory(School\Rank::class)->make([
+                            'semester' => $sem,
+                            'position' => $pos
                         ]);
 
-                        $score->student()->associate($student);
-                        $score->subject()->associate($subject);
+                        $rank->student()->associate($student);
+                        $rank->save();
 
-                        $score->save();
+                        foreach ($subjects as $subject) {
+                            $score = factory(School\Score::class)->make([
+                                'semester' => $sem
+                            ]);
+
+                            $score->student()->associate($student);
+                            $score->subject()->associate($subject);
+
+                            $score->save();
+                        }
                     }
-                }
 
-                $pos++;
-            });
+                    $pos++;
+                });
+        }
     }
 }
